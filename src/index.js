c4fc82d73b9c98488be439bb1d98c38ac8efdedb
Denis Sokolov <denalfek@gmail.com>
2020-08-21 21:14:39 +0300

    initialize project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//https://swapi.dev
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/* ToDo:
+- Axios
+- Superagent
+- Got
+- Request
+- Reqwest
+- XMLHttpRequest
+*/
+ 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class SwapiService {
+  _baseUrl = "https://swapi.dev/api/";
+  _peopleUrl = this._baseUrl + "people/";
+  
+  getError = (url, statusResult) => {
+    return `Could not fetch ${url} recieved ${statusResult}`;
+  };
+
+  async getResource(url) {
+    const res = await fetch(url);
+  
+    if (!res.ok) {
+      throw new Error(this.getError(url, res.status));
+    }
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(this._peopleUrl);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(this._peopleUrl + id);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+  people.forEach((p) => {
+    console.log(p.name)
+  })
+});
+
+
+  swapi.getPerson(1)
+  .then((person) => {
+    console.log(person.mass);
+  });
